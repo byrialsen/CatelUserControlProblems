7801af765d28df12d21135c9be0f7a38b082552a
SERVERS\paw.byrialsen <paw.byrialsen@stevanatogroup.com>
2018-09-10 13:58:00 +0200

    Tested changing to compiled binding

--- CatelTest.Uwp.Ui/Views/PersonView.xaml.cs
+++ CatelTest.Uwp.Ui/Views/PersonView.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace CatelTest.Uwp.Ui.Views
+﻿using CatelTest.Uwp.Ui.ViewModels;
+
+namespace CatelTest.Uwp.Ui.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class PersonView : Catel.Windows.Controls.Page
     {
+        public PersonViewModel VM { get => DataContext as PersonViewModel; } 
+
         public PersonView()
         {
             this.InitializeComponent();
