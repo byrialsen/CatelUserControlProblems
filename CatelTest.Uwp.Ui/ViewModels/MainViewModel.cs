4b3f892989d41d90cebbe21ef9e30e1acb989acc
SERVERS\paw.byrialsen <paw.byrialsen@stevanatogroup.com>
2019-02-13 10:11:05 +0100

    Downgraded to 5.7.0 and add F5 shortcut

--- CatelTest.Uwp.Ui/ViewModels/MainViewModel.cs
+++ CatelTest.Uwp.Ui/ViewModels/MainViewModel.cs
@@ -1,8 +1,21 @@
-﻿using Catel.MVVM;
+﻿using Catel;
+using Catel.MVVM;
+using Catel.Windows.Input;
+using Windows.System;
 
 namespace CatelTest.Uwp.Ui.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        private readonly ICommandManager _commandManager;
+
+        public MainViewModel(ICommandManager commandManager)
+        {
+            Argument.IsNotNull(() => commandManager);
+
+            _commandManager = commandManager;
+
+            _commandManager.CreateCommand("TestCommand", new InputGesture(VirtualKey.F5));
+        }
     }
 }
--- CatelTest.Uwp.Ui/ViewModels/PersonControlViewModel.cs
+++ CatelTest.Uwp.Ui/ViewModels/PersonControlViewModel.cs
@@ -1,29 +1,39 @@
 ﻿using System.Threading.Tasks;
+using Catel;
 using Catel.MVVM;
 
 namespace CatelTest.Uwp.Ui.ViewModels
 {
     public class PersonControlViewModel : ViewModelBase
     {
+        private readonly ICommandManager _commandManager;
+
         public string FirstNames { get; set; }
         public string LastNames { get; set; }
 
-        public PersonControlViewModel()
+        public PersonControlViewModel(ICommandManager commandManager)
         {
+            Argument.IsNotNull(() => commandManager);
+
+            _commandManager = commandManager;
+
+
+            FirstNames = "Paw Hauge";
+            LastNames = "Byrialsen";
+
             TestCommand = new Command(
                 () =>
                 {
                     System.Diagnostics.Debug.WriteLine($"{FirstNames} {LastNames}");
                 });
+
+            _commandManager.RegisterCommand("TestCommand", TestCommand);
         }
 
         public Command TestCommand { get; set; }
 
         protected override Task InitializeAsync()
         {
-            FirstNames = "Paw Hauge";
-            LastNames = "Byrialsen";
-
             return base.InitializeAsync();
         }
     }
